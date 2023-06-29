5770a7d533b37578aecd926bc2fc50d668f4a11b
bryanstark1 <bryandstark@icloud.com>
2023-06-29 16:24:40 -0400

    user can buy scissors

--- game.py
+++ game.py
@@ -1,12 +1,19 @@
-wallet = 0
+wallet = 5
 dayCount = 1
 tools = [
-    {'name': 'teeth',
+    {'name': 'Teeth',
      'earn': 1,
      'cost': 0
-     },
+    }
+]
+store = [
+    {'name': 'Rusty Scissors',
+     'earn': 5,
+     'cost': 5
+    }
 ]
 use_tool = tools[0]
+
 
 def day():
     global wallet
@@ -15,12 +22,44 @@ def day():
     dayCount += 1
     print('Wallet = $' + str(wallet) + '\n')
 
+
+def shop():
+    print('\n***************\nTools Available\n***************\n')
+    while len(store) > 0 and store[0]['cost'] <= wallet:
+      for tool in store:
+          if tool['cost'] <= wallet:
+              listNum = 1
+              print(str(listNum) + ') ' + tool['name'] + '\n-Cost $' + str(tool['cost']) + '\n-Earn $' + str(tool['earn']) + '\n')
+              listNum += 1
+      prompt = input('Which number tool would you like to purchase? \nOr press "C" to cancel: ')
+      if prompt == 'c' or prompt == 'C':
+          break
+      prompt = int(prompt) - 1
+      cost = store[prompt]['cost']
+      if wallet >= cost:
+          print('You purchased ' + store[prompt]['name'])
+          tools.append(store[prompt])
+          del store[prompt]
+          print(store)
+      else:
+          print("You don't have enough money to but that tool!")
+
+
 def game():
-    checkStudents = next((item for item in tools if item['name'] == 'students'), False)
-    while wallet < 10 or checkStudents == False:
-      print("Day " + str(dayCount) + " of landscaping. You're using " + use_tool['name'])
-      prompt = input('Are you ready to cut grass for the day? y/n: ')
-      if prompt == 'y':
-        day()
+    # https://stackoverflow.com/questions/8653516/search-a-list-of-dictionaries-in-python
+    checkStudents = next((item for item in tools if item['name'] == 'Students'), False)
+    while wallet < 20 or checkStudents == False:
+        # If store has tools and wallet has enough money, prompt user to shop
+        if len(store) > 0 and wallet >= store[0]['cost']:
+            prompt = input('Do you want to shop for tools? y/n ')
+            # Call shop function if user wants to shop
+            if prompt == 'y' or prompt == 'Y':
+              shop()
+        print("Day " + str(dayCount) + " of landscaping. You're using " + use_tool['name'])
+        prompt = input('Are you ready to cut grass for the day? y/n: ')
+        if prompt == 'y':
+            day()
+
 
 game()
+# shop()
