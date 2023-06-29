d40686096fbb57110a57a61110f8ac059bde528d
bryanstark1 <bryandstark@icloud.com>
2023-06-29 17:23:52 -0400

    All tools added to store, wallett successfully deducts cost of tool after purchase

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-wallet = 5
+wallet = 500
 dayCount = 1
 tools = [
     {'name': 'Teeth',
@@ -10,6 +10,18 @@ store = [
     {'name': 'Rusty Scissors',
      'earn': 5,
      'cost': 5
+    },
+    {'name': 'Push Mower',
+     'earn': 50,
+     'cost': 25
+    },
+    {'name': 'Fancy Mower',
+     'earn': 100,
+     'cost': 250
+    },
+    {'name': 'Students',
+     'earn': 250,
+     'cost': 500
     }
 ]
 use_tool = tools[0]
@@ -24,28 +36,49 @@ def day():
 
 
 def shop():
-    print('\n***************\nTools Available\n***************\n')
+    global wallet
+    print('\n****************************\nTools Available for Purchase\n****************************\n')
     while len(store) > 0 and store[0]['cost'] <= wallet:
+      listNum = 1
       for tool in store:
           if tool['cost'] <= wallet:
-              listNum = 1
               print(str(listNum) + ') ' + tool['name'] + '\n-Cost $' + str(tool['cost']) + '\n-Earn $' + str(tool['earn']) + '\n')
               listNum += 1
       prompt = input('Which number tool would you like to purchase? \nOr press "C" to cancel: ')
       if prompt == 'c' or prompt == 'C':
           break
-      prompt = int(prompt) - 1
-      cost = store[prompt]['cost']
-      if wallet >= cost:
-          print('You purchased ' + store[prompt]['name'])
-          tools.append(store[prompt])
-          del store[prompt]
-          print(store)
-      else:
-          print("You don't have enough money to but that tool!")
+      if int(prompt) <= len(store):
+          prompt = int(prompt) - 1
+          cost = store[prompt]['cost']
+          if wallet >= cost:
+              wallet -= store[prompt]['cost']
+              print('\nYou purchased ' + store[prompt]['name'] + '\n')
+              tools.append(store[prompt])
+              del store[prompt]
+          else:
+              print("You don't have enough money to but that tool!")
+
+
+def chooseTool():
+    print('\n***************\nTools Available\n***************\n')
+    listNum = 1
+    for tool in tools:
+        print(str(listNum) + ') ' + tool['name'] + '\n-Earn $' + str(tool['earn']) + '\n')
+        listNum = int(listNum) + 1
+    prompt = input('Which number tool do you want to use? ')
+    while int(prompt) > len(tools):
+        print('That is not an option, please choose again.')
+        prompt = input('Which number tool do you want to use? ')
+    if int(prompt) <= len(tools):
+        prompt = int(prompt)
+        prompt -= 1
+        use_tool = tools[prompt]
+    else:
+        print('That is not an option, please choose again')
 
 
 def game():
+    print('\n*************************************************************\n*****************  Welcome to Landscaper!  ******************\n*  Your starting tool is teeth, which earns you $1 per day  *\n*************************************************************\n')
     # https://stackoverflow.com/questions/8653516/search-a-list-of-dictionaries-in-python
     checkStudents = next((item for item in tools if item['name'] == 'Students'), False)
     while wallet < 20 or checkStudents == False:
@@ -53,8 +86,13 @@ def game():
         if len(store) > 0 and wallet >= store[0]['cost']:
             prompt = input('Do you want to shop for tools? y/n ')
             # Call shop function if user wants to shop
-            if prompt == 'y' or prompt == 'Y':
+            prompt.lower()
+            if prompt == 'y':
               shop()
+        if len(tools) > 0:
+            prompt = input('Do you want to change tools? y/n ')
+            if prompt == 'y':
+              chooseTool()
         print("Day " + str(dayCount) + " of landscaping. You're using " + use_tool['name'])
         prompt = input('Are you ready to cut grass for the day? y/n: ')
         if prompt == 'y':
